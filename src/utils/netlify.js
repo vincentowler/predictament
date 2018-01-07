1a6da6d65f1df1d402b4b1e54d1d66d02ac45913
Cory House <Cory.House@coxautoinc.com>
2018-01-07 09:56:56 -0600

    Centralize netlify code

---
+++ src/utils/netlify.js
@@ -0,0 +1,19 @@
+// These functions are useful for sending data to Netlify.
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
+
+// Send data to Netlify to store in their form feature
+// More info at https://www.netlify.com/docs/form-handling/
+// Example: https://github.com/imorente/gatsby-netlify-form-example
+export function sendDataToNetlify(formName, data) {
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: encode({ "form-name": formName, ...data })
+  })
+    .then(() => alert("Success!"))
+    .catch(error => alert(error));
+}
