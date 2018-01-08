e4426cd44cc465cebda7dbd893501f02cdbab3e9
Cory House <Cory.House@coxautoinc.com>
2018-01-07 18:02:12 -0600

    Centralize config data

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const tournamentScenarios = [
+  {
+    id: 1,
+    topic: "Income",
+    options: ["$50,000", "$75,000", "$100,000", "$150,000", "$200,000"],
+    bonusQuestion: "guess this person's exact income."
+  },
+  {
+    id: 2,
+    topic: "Education",
+    options: ["High School", "Associates", "Bachelors", "Masters", "Doctorate"],
+    bonusQuestion: "guess the high education received by this person."
+  }
+];
+
+export const profiles = [
+  {
+    Age: 42,
+    Weight: "200 pounds",
+    Race: "White",
+    Industry: "Software",
+    Education: "Masters"
+  },
+  {
+    Age: 22,
+    Weight: "150 pounds",
+    Race: "Black",
+    Industry: "Architecture",
+    Education: "Doctorate"
+  },
+  {
+    Age: 32,
+    Weight: "170 pounds",
+    Race: "Asian",
+    Industry: "Mechanical Engineering",
+    Education: "High School"
+  }
+];
