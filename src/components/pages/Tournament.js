523acd14241db9aabc06dac6788546ac87b4ac3d
Cory House <Cory.House@coxautoinc.com>
2018-01-07 18:28:00 -0600

    Refactor to write data to forms

--- src/components/pages/Tournament.js
+++ src/components/pages/Tournament.js
@@ -3,29 +3,22 @@ import Subject from "../Subject";
 import ItemList from "../ItemList";
 import Button from "react-bootstrap/lib/Button";
 import TextInput from "../TextInput";
+import { sendDataToNetlify } from "../../utils/netlify";
+import { tournamentScenarios, profiles } from "../../data";
+
+// for now, just hard coding to first scenario and profile.
+const initialScenario = tournamentScenarios[0];
+const profile = profiles[0];
 
 class Tournament extends React.Component {
   state = {
-    attributes: {
-      Age: 42,
-      Weight: "200 pounds",
-      Race: "White",
-      Industry: "Software",
-      Education: "Masters"
-    },
-    xAxis: "Income",
-    data: [
-      { id: 1, label: "$50,000", tokens: 0 },
-      { id: 2, label: "$75,000", tokens: 0 },
-      { id: 3, label: "$100,000", tokens: 0 },
-      { id: 4, label: "$150,000", tokens: 0 },
-      { id: 5, label: "$200,000", tokens: 0 }
-    ],
-    wagerSubmitted: false,
-    bonusQuestion: {
-      label: "For an additional bonus, guess this person's exact income.",
-      value: ""
-    }
+    profile: profile,
+    scenario: initialScenario,
+    options: initialScenario.options.map(option => {
+      return { label: option, tokens: 0 };
+    }),
+    bonusQuestionValue: "",
+    wagerSubmitted: false
   };
 
   MAX_TOKENS = 3;
@@ -37,106 +30,125 @@ class Tournament extends React.Component {
   getTokensLeft() {
     return (
       this.MAX_TOKENS -
-      this.state.data.map(obj => obj.tokens).reduce((acc, curr) => acc + curr)
+      this.state.options
+        .map(option => option.tokens)
+        .reduce((acc, curr) => acc + curr)
     );
   }
 
   handleAddClick = (item, event) => {
     event.preventDefault();
     item.tokens++;
-    // Use map to return a new array, but with the item passed item replaced
-    const data = this.state.data.map(d => (d.id === item.id ? item : d));
-    this.setState({ data });
+    this.updateState(item);
   };
 
   handleRemoveClick = (item, event) => {
     event.preventDefault();
     item.tokens--;
+    this.updateState(item);
+  };
+
+  updateState = item => {
     // Use map to return a new array, but with the item passed item replaced
     this.setState(state => {
-      const data = state.data.map(d => (d.id === item.id ? item : d));
-      return { data };
+      const options = state.options.map(
+        option => (option.label === item.label ? item : option)
+      );
+      return { options };
     });
   };
 
-  handleWagerSubmit = e => {
+  handleSubmit = e => {
     e.preventDefault();
-    this.setState({ wagerSubmitted: true });
-  };
-
-  handleBonusSubmit = e => {
-    e.preventDefault();
-    this.props.showPage(5);
+    if (this.state.wagerSubmitted) {
+      sendDataToNetlify("tournament", {
+        workerId: this.props.workerId,
+        email: this.props.email,
+        ...this.props.background,
+        scenarioId: this.state.scenario.id,
+        wagerDistribution: this.state.options
+          .map(option => option.tokens)
+          .join(","),
+        bonusQuestionValue: this.state.bonusQuestionValue
+      });
+      this.props.showPage(5);
+    } else {
+      this.setState({ wagerSubmitted: true });
+    }
   };
 
   handleBonusChange = e => {
-    this.setState({ bonusQuestion: e.target.value });
+    this.setState({ bonusQuestionValue: e.target.value });
   };
 
   render() {
     const tokensLeft = this.getTokensLeft();
     const {
-      xAxis,
-      attributes,
-      data,
-      bonusQuestion,
+      topic,
+      profile,
+      scenario,
+      options,
+      bonusQuestionValue,
       wagerSubmitted
     } = this.state;
     return (
-      <div>
+      <form
+        className={this.props.visible ? "" : "hidden"}
+        name="tournament"
+        onSubmit={this.handleSubmit}
+        method="post"
+        data-netlify="true"
+      >
         <h2>What is this person's income?</h2>
         <p>
           Click the -/+ signs below to place wagers on this person's income.
         </p>
         <p>You have {this.MAX_TOKENS} tokens to assign.</p>
         <div className="token-wrapper">
-          <form onSubmit={this.handleWagerSubmit}>
-            <h3>Profile</h3>
-            <Subject attributes={attributes} />
-            <ItemList
-              xAxis={xAxis}
-              data={data}
-              tokensLeft={tokensLeft}
-              onAddClick={this.handleAddClick}
-              onRemoveClick={this.handleRemoveClick}
-            />
-            <p>You have {tokensLeft} tokens left.</p>
+          <h3>Profile</h3>
+          <Subject profile={profile} />
+          <ItemList
+            topic={scenario.topic}
+            options={options}
+            tokensLeft={tokensLeft}
+            onAddClick={this.handleAddClick}
+            onRemoveClick={this.handleRemoveClick}
+          />
+          <p>You have {tokensLeft} tokens left.</p>
 
-            {!wagerSubmitted && (
-              <Button
-                className="btn btn-primary center-block"
-                bsSize="lg"
-                type="submit"
-                disabled={this.getTokensLeft() > 0}
-                title={`You must assign all ${
-                  this.MAX_TOKENS
-                } tokens to continue.`}
-              >
-                Continue
-              </Button>
-            )}
-          </form>
-
-          {wagerSubmitted && (
-            <form onSubmit={this.handleBonusSubmit}>
-              <TextInput
-                id="bonusQuestion"
-                name="bonusQuestion"
-                onChange={this.handleBonusChange}
-                label={bonusQuestion.label}
-                value={bonusQuestion.value}
-              />
-              <Button
-                className="btn btn-primary center-block"
-                bsSize="lg"
-                type="submit"
-              >
-                Save Wager
-              </Button>
-            </form>
+          {!wagerSubmitted && (
+            <Button
+              className="btn btn-primary center-block"
+              bsSize="lg"
+              type="submit"
+              disabled={this.getTokensLeft() > 0}
+              title={`You must assign all ${
+                this.MAX_TOKENS
+              } tokens to continue.`}
+            >
+              Continue
+            </Button>
           )}
+
+          {/* Must use CSS to hide so Netflify can see the form field */}
+          <div className={wagerSubmitted ? null : "hidden"}>
+            <TextInput
+              id="bonusQuestion"
+              name="bonusQuestion"
+              onChange={this.handleBonusChange}
+              label={`For an additional bonus, ${scenario.bonusQuestion}`}
+              value={bonusQuestionValue}
+            />
+            <Button
+              className="btn btn-primary center-block"
+              bsSize="lg"
+              type="submit"
+            >
+              Save Wager
+            </Button>
+          </div>
         </div>
-      </div>
+      </form>
     );
   }
 }
