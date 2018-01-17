966df5d93ca8e91a47b3fc93bb3865c3b272dc1d
Cory House <Cory.House@coxautoinc.com>
2018-01-16 21:31:31 -0600

    Rename and hide extra fields

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,13 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import Table from "react-bootstrap/lib/Table";
 
-function Subject({ profile }) {
+function Profile({ profile }) {
   return (
     <Table condensed hover>
       <tbody>
         {Object.keys(profile).map(key => {
           return (
-            <tr key={key}>
+            <tr
+              key={key}
+              className={
+                key === "ProfileId" || key === "profileNumber" ? "hidden" : ""
+              }
+            >
               <td>{key}</td>
               <td>{profile[key]}</td>
             </tr>
@@ -19,8 +24,8 @@ function Subject({ profile }) {
   );
 }
 
-Subject.propTypes = {
+Profile.propTypes = {
   profile: PropTypes.object.isRequired
 };
 
-export default Subject;
+export default Profile;
