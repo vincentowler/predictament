5bd9d179a998c00e9ef33e014ea8dcbe6b405bba
vincentowler <vincentowler@outlook.com>
2018-06-13 20:18:22 -0700

    Centralize formName

--- src/utils/netlify.js
+++ src/utils/netlify.js
@@ -8,12 +8,13 @@ function encode(data) {
 // Send data to Netlify to store in their form feature
 // More info at https://www.netlify.com/docs/form-handling/
 // Example: https://github.com/imorente/gatsby-netlify-form-example
-export function sendDataToNetlify(formName, data) {
+export function sendDataToNetlify(data) {
+  const formName = "tournament"; // TODO: Fork this based on URL (write to test-tournament from test.predictament.com)
   fetch("/", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: encode({ "form-name": formName, ...data })
   })
-    .then(() => alert("Wager saved successfully."))
+    .then(() => alert("Saved to Netlify. 👍"))
     .catch(error => alert(error));
 }
