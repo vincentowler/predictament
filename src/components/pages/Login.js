e363237f4e5d9f85a1d3d744c4b8f87ee5941415
vincentowler <vincentowler@outlook.com>
2018-06-16 22:34:21 -0500

    Refactor to support separate user and wager forms

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,9 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { sendDataToNetlify } from "../../utils/netlify";
 import { isEmpty } from "../../utils/objectUtils";
-import LoginForm from "../LoginForm";
 import { validateLogin } from "../../utils/validation";
+import TextInput from "../TextInput";
+import Modal from "react-bootstrap/lib/Modal";
+import Button from "react-bootstrap/lib/Button";
+import Checkbox from "react-bootstrap/lib/Checkbox";
 
 class Login extends React.Component {
   static propTypes = {
@@ -14,26 +16,7 @@ class Login extends React.Component {
   };
 
   state = {
-    showTerms: false,
-    errors: {}
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const errors = validateLogin(workerId, acceptedTerms);
-    this.setState({ errors });
-
-    if (isEmpty(errors)) {
-      const data = {
-        workerId: this.props.workerId,
-        acceptedTerms: true,
-        email: this.props.email,
-        complete: "N"
-      };
-
-      sendDataToNetlify("login", data);
-      this.props.showPage(2);
-    }
+    showTerms: false
   };
 
   toggleTerms = e => {
@@ -44,21 +27,91 @@ class Login extends React.Component {
   };
 
   render() {
+    const {
+      workerId,
+      errors,
+      email,
+      onChange,
+      acceptedTerms,
+      visible
+    } = this.props;
+    const { showTerms } = this.state;
     return (
-      <div>
+      <div className={visible ? null : "hidden"}>
         <h1>Cornell University Expectations Study</h1>
-        <form
-          name="login"
-          method="post"
-          data-netlify="true"
-          onSubmit={this.handleSubmit}
-        >
-          <LoginForm
-            toggleTerms={this.toggleTerms}
-            {...this.props}
-            {...this.state}
-          />
-        </form>
+        {/* TODO: Can I look this up to validate? */}
+        <TextInput
+          label="MTurk Worker ID"
+          name="workerId"
+          id="workerId"
+          value={workerId}
+          onChange={onChange}
+          error={errors.workerId}
+          required
+        />
+        <TextInput
+          label="Email address"
+          name="email"
+          id="email"
+          value={email}
+          onChange={onChange}
+          helpBlock="Optional. Enter if you want a report with your results. No spam."
+          error={errors.email}
+        />
+
+        <div className={errors.acceptedTerms ? "field has-error" : "field"}>
+          <Checkbox
+            id="acceptedTerms"
+            onChange={onChange}
+            name="acceptedTerms"
+            checked={acceptedTerms}
+          >
+            {" "}
+            I accept the{" "}
+            <a href="#terms-and-conditions" onClick={this.toggleTerms}>
+              terms and conditions
+            </a>
+          </Checkbox>
+          {errors.acceptedTerms && (
+            <span className="help-block">
+              You must accept the terms to continue.
+            </span>
+          )}
+          <div className="static-modal">
+            <Modal show={showTerms}>
+              <Modal.Header>
+                <Modal.Title>Terms and Conditions</Modal.Title>
+              </Modal.Header>
+
+              <Modal.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Modal.Body>
+
+              <Modal.Footer>
+                <Button bsStyle="primary" onClick={this.toggleTerms}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
+        </div>
+        <div>
+          <Button
+            className="btn btn-primary center-block"
+            bsSize="lg"
+            type="submit"
+          >
+            Begin
+          </Button>
+          {/* This field is necessary so we can send a bool to Netlify that is used to filter out incomplete records. */}
+          <input type="text" className="hidden" id="complete" name="complete" />
+        </div>
       </div>
     );
   }
