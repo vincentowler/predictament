8016aff259fe1bd7dddafe17f207b77f9a4a03c6
Cory House <housecor@gmail.com>
2018-07-29 16:05:22 -0500

    Handle scroll right when many options

--- src/components/Sliders.js
+++ src/components/Sliders.js
@@ -3,9 +3,20 @@ import PropTypes from "prop-types";
 import Slider from "./Slider";
 
 const Sliders = ({ scenario, options, onChange }) => (
-  <div>
-    <h3>{scenario.topic}</h3>
-    <ul style={{ listStyleType: "none", textAlign: "center" }}>
+  <div
+    style={{
+      overflow: "scroll",
+      marginLeft: 0
+    }}
+  >
+    <ul
+      style={{
+        listStyleType: "none",
+        textAlign: "center",
+        whiteSpace: "nowrap",
+        padding: 0
+      }}
+    >
       {options.map(option => {
         return (
           <li key={option.label} style={{ display: "inline-block" }}>
