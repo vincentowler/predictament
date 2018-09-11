1c72992eb2522d001f38111bc5a516a4668deb92
vincentowler <vincentowler@outlook.com>
2018-09-10 21:25:00 -0500

    Fix key

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,18 @@
 import React from "react";
 
-const Home = ({ scenarios }) => {
-  return (
-    <div>
-      <h1>Cornell University Expectations Study</h1>
+const Home = ({ scenarios }) => (
+  <div>
+    <h1>Cornell University Expectations Study</h1>
 
-      <p>Select a tournament.</p>
-      <ul>
-        {scenarios.map(scenario => (
-          <li key={scenario.id}>
-            <a href={`/?scenarioId=${scenario.scenarioId}`}>{scenario.topic}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+    <p>Select a tournament.</p>
+    <ul>
+      {scenarios.map(scenario => (
+        <li key={scenario.scenarioId}>
+          <a href={`/?scenarioId=${scenario.scenarioId}`}>{scenario.topic}</a>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 export default Home;
