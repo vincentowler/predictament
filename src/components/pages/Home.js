49de06fcd916e0a60eac0c4f36ade4e6590225d2
Cory House <housecor@gmail.com>
2018-09-10 21:32:04 -0500

    Testing netlify PR handling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = ({ scenarios }) => (
   <div>
-    <h1>Cornell University Expectations Study</h1>
+    <h1>Cornell University Expectations Study - test</h1>
 
     <p>Select a tournament.</p>
     <ul>
