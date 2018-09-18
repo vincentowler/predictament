695fde1a8102ac89a0256bc05f4ff67010aca1a4
Cory House <housecor@gmail.com>
2018-09-18 18:38:56 -0500

    Write to forms that start with test when not in prod

--- src/utils/netlify.js
+++ src/utils/netlify.js
@@ -9,11 +9,13 @@ function encode(data) {
 // More info at https://www.netlify.com/docs/form-handling/
 // Example: https://github.com/imorente/gatsby-netlify-form-example
 export function sendDataToNetlify(formName, data) {
+  // In non-prod environments, write to forms that begin with the word test to avoid polluting prod data.
+  const envFormName = process.ENV.production ? formName : "test" + formName;
   fetch("/", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: encode({ "form-name": formName, ...data })
+    body: encode({ "form-name": envFormName, ...data })
   })
-    .then(() => console.log("saving " + formName, data))
+    .then(() => console.log("saving " + envFormName, data))
     .catch(error => alert(error));
 }
