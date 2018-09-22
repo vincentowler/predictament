021c06221e56675e6dc072a026766db282ecb384
vincentowler <vincentowler@outlook.com>
2018-09-22 08:17:39 -0500

    Move terms to bottom to make Netlify happy

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -103,130 +103,22 @@ class Login extends React.Component {
           className="hidden"
         />
 
-        <TextInput
-          label="What is your age?"
-          id="age"
-          name="age"
-          type="number"
-          value={user.age}
-          onChange={onChange}
-          error={errors.age}
-        />
-
-        <div className={errors.acceptedTerms ? "field has-error" : "field"}>
-          <Checkbox
-            id="acceptedTerms"
-            onChange={onChange}
-            name="acceptedTerms"
-            checked={user.acceptedTerms}
-          >
-            {" "}
-            I accept the{" "}
-            <a href="#terms-and-conditions" onClick={this.toggleTerms}>
-              terms and conditions
-            </a>
-          </Checkbox>
-          {errors.acceptedTerms && (
-            <span className="help-block">
-              You must accept the terms to continue.
-            </span>
-          )}
-          <div className="static-modal">
-            <Modal show={showTerms}>
-              <Modal.Header>
-                <Modal.Title>
-                  Consent to Participate in an Online Research Study
-                </Modal.Title>
-              </Modal.Header>
-
-              <Modal.Body>
-                <h2>Project Title: Cornell Expectations Study</h2>
-                <p>
-                  We are asking you to participate in an academic not-for-profit
-                  research study. This form is designed to give you information
-                  about this study. We will describe this study to you and
-                  answer any of your questions.
-                </p>
-                <p>
-                  What the study is about: The purpose of this study is to learn
-                  about people’s expectations regarding education, the labor
-                  market, and quality of life. What we will ask you to do: This
-                  study involves responding to a background survey (5 minutes)
-                  participating making 30 predictions about anonymous workers in
-                  the labor force. This study should take about 30 minutes to
-                  complete.
-                </p>
-                <h4>Risks and discomfort:</h4>
-                <p>
-                  The primary risk associated with this study is a possible
-                  invasion of your privacy. Though every reasonable effort has
-                  been taken, confidentiality during actual Internet
-                  communication procedures cannot be guaranteed.
-                </p>
-                <h4>Benefits:</h4>
-                <p>
-                  You may find participation in the prediction tournament a
-                  useful introspective exercise.
-                </p>
-                <h4>Compensation:</h4>
-                <p>
-                  You will receive $0.50 for completing the background survey
-                  and prediction tournament. You will also receive up to $0.05
-                  for each of the 30 profiles you are asked to make predictions
-                  about, based on the accuracy of your predictions. You will
-                  receive at least $0.50 for completing the study and could earn
-                  up to $1.50 in bonus payments.
-                </p>
-                <h4>Your information will be confidential:</h4>
-                <p>
-                  We will make every reasonable effort to keep your information
-                  private. We will not collect or share any identifying
-                  information about you including your name, phone number,
-                  email, or any other information that will make it possible to
-                  identify you. However, no guarantees can be made regarding the
-                  interception of data sent via the Internet by any third
-                  parties.
-                </p>
-                <h4>Taking part is voluntary:</h4>{" "}
-                <p>
-                  Taking part in this study is completely voluntary. You may
-                  skip any questions that you do not want to answer on the quiz
-                  or survey. If you choose to skip survey and quiz questions or
-                  discontinue the lecture, it will not affect your current or
-                  future relationship with Cornell University. If you decide to
-                  take part, you are free to withdraw at any time. However, you
-                  will only be compensated once you have fully completed the
-                  survey and prediction tournament questions.
-                </p>
-                <h4>If you have questions:</h4>
-                <p>
-                  The researchers conducting this study is Jeffrey Swigert and
-                  Mike Lovenheim. Please direct any questions to Jeff Swigert at
-                  jms858@cornell.edu or 435-865-8239. If you have any questions
-                  or concerns regarding your rights as a subject in this study,
-                  you may contact the Institutional Review Board (IRB) at
-                  607-255-5138 or access their website at
-                  http://www.irb.cornell.edu. You may also report your concerns
-                  or complaints anonymously through www.hotline.cornell.edu or
-                  by calling toll free at 1-866-293-3077. Ethicspoint is an
-                  independent organization that serves as a liaison between the
-                  University and the person bringing the complaint so that
-                  anonymity can be ensured.
-                </p>
-              </Modal.Body>
-
-              <Modal.Footer>
-                <Button bsStyle="primary" onClick={this.toggleTerms}>
-                  Close
-                </Button>
-              </Modal.Footer>
-            </Modal>
-          </div>
-        </div>
-
         <h2>Background</h2>
         <ol>
-          {this.questionEnabled(1) && <li />}
+          {this.questionEnabled(1) && (
+            <li>
+              <TextInput
+                label="What is your age?"
+                id="age"
+                name="age"
+                type="number"
+                value={user.age}
+                onChange={onChange}
+                error={errors.age}
+              />
+            </li>
+          )}
+
           {this.questionEnabled(2) && (
             <li>
               <RadioButtonList
@@ -439,6 +331,116 @@ class Login extends React.Component {
           onChange={() => {}}
           className="hidden"
         />
+        <div className={errors.acceptedTerms ? "field has-error" : "field"}>
+          <Checkbox
+            id="acceptedTerms"
+            onChange={onChange}
+            name="acceptedTerms"
+            checked={user.acceptedTerms}
+          >
+            {" "}
+            I accept the{" "}
+            <a href="#terms-and-conditions" onClick={this.toggleTerms}>
+              terms and conditions
+            </a>
+          </Checkbox>
+          {errors.acceptedTerms && (
+            <span className="help-block">
+              You must accept the terms to continue.
+            </span>
+          )}
+          <div className="static-modal">
+            <Modal show={showTerms}>
+              <Modal.Header>
+                <Modal.Title>
+                  Consent to Participate in an Online Research Study
+                </Modal.Title>
+              </Modal.Header>
+
+              <Modal.Body>
+                <h2>Project Title: Cornell Expectations Study</h2>
+                <p>
+                  We are asking you to participate in an academic not-for-profit
+                  research study. This form is designed to give you information
+                  about this study. We will describe this study to you and
+                  answer any of your questions.
+                </p>
+                <p>
+                  What the study is about: The purpose of this study is to learn
+                  about people’s expectations regarding education, the labor
+                  market, and quality of life. What we will ask you to do: This
+                  study involves responding to a background survey (5 minutes)
+                  participating making 30 predictions about anonymous workers in
+                  the labor force. This study should take about 30 minutes to
+                  complete.
+                </p>
+                <h4>Risks and discomfort:</h4>
+                <p>
+                  The primary risk associated with this study is a possible
+                  invasion of your privacy. Though every reasonable effort has
+                  been taken, confidentiality during actual Internet
+                  communication procedures cannot be guaranteed.
+                </p>
+                <h4>Benefits:</h4>
+                <p>
+                  You may find participation in the prediction tournament a
+                  useful introspective exercise.
+                </p>
+                <h4>Compensation:</h4>
+                <p>
+                  You will receive $0.50 for completing the background survey
+                  and prediction tournament. You will also receive up to $0.05
+                  for each of the 30 profiles you are asked to make predictions
+                  about, based on the accuracy of your predictions. You will
+                  receive at least $0.50 for completing the study and could earn
+                  up to $1.50 in bonus payments.
+                </p>
+                <h4>Your information will be confidential:</h4>
+                <p>
+                  We will make every reasonable effort to keep your information
+                  private. We will not collect or share any identifying
+                  information about you including your name, phone number,
+                  email, or any other information that will make it possible to
+                  identify you. However, no guarantees can be made regarding the
+                  interception of data sent via the Internet by any third
+                  parties.
+                </p>
+                <h4>Taking part is voluntary:</h4>{" "}
+                <p>
+                  Taking part in this study is completely voluntary. You may
+                  skip any questions that you do not want to answer on the quiz
+                  or survey. If you choose to skip survey and quiz questions or
+                  discontinue the lecture, it will not affect your current or
+                  future relationship with Cornell University. If you decide to
+                  take part, you are free to withdraw at any time. However, you
+                  will only be compensated once you have fully completed the
+                  survey and prediction tournament questions.
+                </p>
+                <h4>If you have questions:</h4>
+                <p>
+                  The researchers conducting this study is Jeffrey Swigert and
+                  Mike Lovenheim. Please direct any questions to Jeff Swigert at
+                  jms858@cornell.edu or 435-865-8239. If you have any questions
+                  or concerns regarding your rights as a subject in this study,
+                  you may contact the Institutional Review Board (IRB) at
+                  607-255-5138 or access their website at
+                  http://www.irb.cornell.edu. You may also report your concerns
+                  or complaints anonymously through www.hotline.cornell.edu or
+                  by calling toll free at 1-866-293-3077. Ethicspoint is an
+                  independent organization that serves as a liaison between the
+                  University and the person bringing the complaint so that
+                  anonymity can be ensured.
+                </p>
+              </Modal.Body>
+
+              <Modal.Footer>
+                <Button bsStyle="primary" onClick={this.toggleTerms}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
+        </div>
         <div>
           <Button
             className="btn btn-primary center-block"
