c8768b09dc6cd4f1a1aa0741969d7ed9ef74fa72
vincentowler <vincentowler@outlook.com>
2018-10-02 20:49:41 -0500

    Add question 11

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const scenarios = [
     scenarioId: 1,
     topic: "Income",
     options: [
-     "<$5K",
+      "<$5K",
       "$5K-$6K",
       "$7K-$9K",
       "$10K-$11K",
@@ -56,7 +56,8 @@ export const backgroundQuestions = [
   { id: 7, scenarioIds: [1, 2] },
   { id: 8, scenarioIds: [1, 2] },
   { id: 9, scenarioIds: [1, 2] },
-  { id: 10, scenarioIds: [1, 2] }
+  { id: 10, scenarioIds: [1, 2] },
+  { id: 11, scenarioIds: [1, 2] }
 ];
 
 export const profiles = [
