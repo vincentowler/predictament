2948597ecd13fbff18e128bc7aef553d3548acdf
Jeff Swigert <jeffreyswigert@SUU.EDU>
2018-11-08 15:51:49 -0700

    thanks message update

--- src/components/pages/Thanks.js
+++ src/components/pages/Thanks.js
@@ -4,8 +4,8 @@ const Thanks = ({ email }) => (
   <div>
     <h1>Thanks for participating!</h1>
     Your responses have been saved so you can safely close your browser.
-    {email && ` We'll send the results to ${email} soon.`} We appreciate your
-    input. 😃
+    {email && ` We'll send the results to the email you provided soon.`} We
+    appreciate your input. 😃
   </div>
 );
 
