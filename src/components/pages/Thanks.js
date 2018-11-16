1f5e3a586e1f4f7f9cfa327e747bec4de3c08e6a
Jeff Swigert <jeffreyswigert@SUU.EDU>
2018-11-16 15:39:24 -0700

    changed some of camelCase and addes some of hidden questions for Netlify.  Still need to do rest.

--- src/components/pages/Thanks.js
+++ src/components/pages/Thanks.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-const Thanks = ({ email }) => (
+const Thanks = ({ email, userId }) => (
   <div>
     <h1>Thanks for participating!</h1>
     Your responses have been saved so you can safely close your browser.
     {email && ` We'll send the results to the email you provided soon.`} We
     appreciate your input. 😃
+    <h2>
+      Here is your compleation code:
+      <h4>{userId}</h4>
+    </h2>
   </div>
 );
 
